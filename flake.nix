c9e010dcf216e86aa36aac8526a0077c5b7ab54c
Charlie Root <charlie@charlieroot.dev>
2024-10-24 01:21:34 +0200

    it werks

--- flake.nix
+++ flake.nix
@@ -11,34 +11,22 @@
   }: let
     systems = ["x86_64-linux" "aarch64-linux"];
     forEachSystem = nixpkgs.lib.genAttrs systems;
-
     pkgsForEach = nixpkgs.legacyPackages;
-  in {
-    packages = forEachSystem (system: rec {
-      default = to-json;
-      to-json = pkgsForEach.${system}.stdenv.mkDerivation {
+
+    to-json = forEachSystem (system:
+      pkgsForEach.${system}.stdenv.mkDerivation {
         name = "cl-to-json";
         src = ./.;
         nativeBuildInputs = with pkgsForEach.${system}; [
           (sbcl.withPackages (ps: with ps; [yason alexandria]))
         ];
         buildPhase = ''
-          # mkdir -p $out
           sbcl --script main.lisp > $out
         '';
-      };
-      to-nix = let
-        jeson = builtins.fromJSON (builtins.toString (builtins.readFile to-json));
-      in
-        pkgsForEach.${system}.stdenv.mkDerivation {
-          name = "json-to-nix";
-          src = ./.;
-          buildPhase = ''
-            cat "${jeson}" > $out
-          '';
-        };
-    });
+      });
 
+    jeson = builtins.fromJSON (builtins.toString (builtins.readFile to-json));
+  in {
     devShells.x86_64-linux.default = nixpkgs.legacyPackages.x86_64-linux.mkShell {
       buildInputs = with nixpkgs.legacyPackages.x86_64-linux; [
         (sbcl.withPackages (ps: with ps; [yason alexandria]))
@@ -46,7 +34,10 @@
     };
 
     nixosConfigurations."test" = nixpkgs.legacyPackages.x86_64-linux.lib.nixosSystem {
-      system = "x86_64-linux"
-    }
+      system = "x86_64-linux";
+      modules = [
+        jeson
+      ];
+    };
   };
 }
--- main.lisp
+++ main.lisp
@@ -3,11 +3,7 @@
 (asdf:load-system 'yason)
 
 (yason:encode
-
 (alexandria:plist-hash-table
                  '("y" 1 "x" (7 8 9))
                  :test #'equal)
-                 ;         (list (alexandria:plist-hash-table
-                 ; '("foo" 1 "bar" (7 8 9))
-                 ; :test #'equal))
           *standard-output*)
